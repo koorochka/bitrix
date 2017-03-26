ab85074edfa2eb5f109456cc51f8a2fe63035e52
Артём Курочка < >
2017-03-26 18:58:06 +0300

    Branding site close file

---
+++ php_interface/site_closed.php
@@ -0,0 +1,7 @@
+Для всех языков 
+bitrix/php_interface/include/ 
+
+Положи свой файл site_closed.php 
+
+Если на определённый язык, то положи в 
+bitrix/php_interface/ru/ или bitrix/php_interface/en/ 
